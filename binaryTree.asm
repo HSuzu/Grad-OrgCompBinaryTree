469bd4c32731ad1d413396298e7c7990ece2285b
henry.shinji@gmail.com <henry.shinji@gmail.com>
2017-10-04 22:32:27 -0300

    Implementes Binary Tree and some order (dosn't know for sure huehuehuehue)

--- binaryTree.asm
+++ binaryTree.asm
@@ -1,67 +1,127 @@
+# Tree:
+#	[0 - 3] -> address
+#	[4 - 7] -> number of nodes
+# Block:
+#	[0 - 3] -> elem
+#	[4 - 7] -> left tree
+#	[8 - 12] -> right tree
+
 .data
-	.align 2
+	   	.align 2
+blocksize: 	.word 12
+elemPrompt:	.asciiz "Digite o novo elemento: "
+newLine:	.asciiz "\n"
 
 .text
-
 main:
+	jal createBinaryTree
+	
+	lw $a0, 0($v0)
+	
+	jal postorder
 
+	li $v0, 10	# exit
+	syscall
 
+# return $v0 tree address
 createBinaryTree:
-
-
-# $a0 : tree address
-# $a1 : vector size
-preorder:
-	addi $sp, $sp, -4	# prepare to add a new element on stack
-	sw $ra, 0($sp)		# add the return adress on stack
-
-	lw $t2, 0($a0)
-
-	sw $zero, 0($t0)	# initiates $t0 = 0
-				# $t0 will point to the next element
-	sw $zero, 0($t1)	# initiates $t1 = 0
-				# $t1 will point to the current element
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
 	
-preorderLoop:
-	bge $t1, $a1, preorderRetrieve	# if the pointer of the current element
-					# is pointing to outside of the vector
-					# jump to Retrieve
-	
-	add $t0, $t1, $t1	# $t0 = 2*$t1
-
-	addi $t0, $t0, 1	# $t0 = 2*$t1 + 1
-
-	addi $sp, $sp, -4	# prepare to store the current element
-	sw $t1, 0($sp)		# and store it
-
-	addi $sp, $sp, -4	# prepare to store the next element
-	sw $t0, 0($sp)		# and store it
-
-	jal preorderLoop
-	
-	add $t3, $t2, $t1
-	lw $a0, 0($t3)
-	
-	li $v0, 1
+	li $a0, 8
+	li $v0, 9
 	syscall
 	
-	addi $sp, $sp, -4	# prepare to store the current element
-	sw $t1, 0($sp)		# and store it
+	la $t2, ($v0)		# get tree address
+	sw $zero, 0($t2)	# tree address
+	sw $zero, 4($t2)	# set number of elements to zero
+BTLoop:
+	la $a0, elemPrompt	# prompt the user to include a new element
+	li $v0, 4		# print_string call
+	syscall
+	
+	li $v0, 5		# read_int call
+	syscall
+	
+	la $t0, ($v0)		# input elem, $t0 = $v0
+	
+	beq $t0, 0, BTEnd	# if equal to the end input, return tree address
+	
+	la $t3, ($t2)		# address of root (or current block)
+BTFind:
+	lw $t1, 0($t3)		# current adress
+	beq $t1, $zero, BTAdd	# if encounter a leaf, add element
+	
+	la $t3, ($t1)
+	
+	lw $t1, 0($t3)		# current element
+	blt $t0, $t1, BTLess	# if $t0 < $t1 goto BTLess
+	
+	addi $t3, $t3, 8	# set the current block to the right tree
+	j BTFind
 
-	addi $t0, $t0, 1	# $t0 = $t0 + 1
+BTLess:
+	addi $t3, $t3, 4	# set the current block to the left tree
+	j BTFind
+BTAdd:
+	li $a0, 12		# set to get 12bytes from heap
+	li $v0, 9		# sbrk call
+	syscall
+	
+	sw $v0, 0($t3)		# new block address
+
+	sw $t0, 0($v0)		# store the element in the block
+	
+	sw $zero, 4($v0)	# set the address of the left tree to invalid
+	sw $zero, 8($v0)	# set the address of the right tree to invalid
+	
+	lw $t4, 4($t2)
+	addi $t4, $t4, 1
+	
+	sw $t4, 4($t2)
+	
+	j BTLoop
+BTEnd:
+	la $v0, ($t2)		# set the return register to the address
+
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+	jr $ra
+
+# $a0 : tree address
+postorder:
 	addi $sp, $sp, -4
-
-	sw $t0, 0($sp)
-	jal preorderLoop
+	sw $ra, 0($sp)
 	
-preorderRetrieve:
-	addi $sp, $sp, 4
-	sw $sp, 0($ra)
+	beqz $a0, POEnd
 	
-	addi $sp, $sp, 4
-	sw $sp, 0($t0)
+	la $s0, ($a0)
 
+	lw $a0, 4($s0)	# address of the left tree
+	addi $sp, $sp, -4
+	sw $s0, 0($sp)
+	jal postorder
+	lw $s0, 0($sp)
 	addi $sp, $sp, 4
-	sw $sp, 0($t1)
-		
+	
+	lw $a0, 8($s0)	# address of the right tree
+	addi $sp, $sp, -4
+	sw $s0, 0($sp)
+	jal postorder
+	lw $s0, 0($sp)
+	addi $sp, $sp, 4
+
+	li $v0, 1
+	lw $a0, 0($s0)
+	syscall	
+	
+	li $v0, 4
+	la $a0, newLine
+	syscall
+	
+POEnd:
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
 	jr $ra
